2bd8cd757efab52e3887936214eb801e56252eda
Gagan7259 <109654682+Gagan7259@users.noreply.github.com>
2023-04-05 19:59:54 +0530

    js

---
+++ JavaScript/Object/em.js
@@ -0,0 +1,13 @@
+let emp={
+    name:"Manoj",
+    age:25,
+    email:"gagan@gmail.com",
+    loc:["banglore","Manglore"],
+    sal:45000,
+    designtion:"FULL STACK DEVALOPER"
+}
+
+for(emps in emp ){
+    console.log(emp[emps])
+}
+
---
+++ JavaScript/Object/emp.js
@@ -0,0 +1,18 @@
+let emp = {
+    id: 101,
+    name: "Gagan",
+    age: 24,
+    education: "bsc"
+}
+emp.id=100;
+emp.name="Manoj"
+emp.age=34
+emp.loc="Banglore"
+
+let e1={}
+console.log(typeof(emp))
+console.log(emp)
+console.log(emp.name)
+console.log(emp.ok)
+//console.log(emp[1])
+//console.log("ok sir"+ typeof(e1))
